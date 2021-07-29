f28c82e290d8dba2663abf11f97a35eeb4787959
SaiiPraveen <87641528+SaiiPraveen@users.noreply.github.com>
2021-07-29 11:15:22 +0530

    A common greeting

---
+++ Hello.py
@@ -0,0 +1,17 @@
+print("Hello world...!")
+
+list=[5,9,42,85,13,75,34,92]
+
+print(list)
+
+list.sort()
+
+print ("Ascending order:",list)
+
+list.sort(reverse=True)
+
+print("descending order:",list)
+
+print ("Done!")
+
+
